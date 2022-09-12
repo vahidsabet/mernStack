bba29fb5bdf32b2cd7794f4f048a5735a5cf56ad
unknown <vahid.sabetumz@gmail.com>
2022-09-12 20:36:20 +0430

    Creating Event Handler

--- image-list/src/components/SearchInput.js
+++ image-list/src/components/SearchInput.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 class SearchInput extends React.Component {
+
+    onInputChange(event){
+        console.log(event.target.value);
+    }
+
     render(){
         return(
             <div className="ui segment">
                 <form className="ui form" action="">
                     <div className="field">
                         <div className="ui massive icon input">
-                <input type="text" placeholder="سرچ"/>
+                <input type="text" 
+                placeholder="سرچ" 
+                onChange={this.onInputChange}/>
                 <i className="search icon"></i>
                     </div>
                     </div>
