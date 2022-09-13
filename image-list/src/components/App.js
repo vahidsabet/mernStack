92e6c0c8946a6ce702fe1f28a172d6df9b9b6abd
unknown <vahid.sabetumz@gmail.com>
2022-09-13 12:42:28 +0430

    Async Await axios in react

--- image-list/src/components/App.js
+++ image-list/src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import axios from "axios";
 import SearchInput from "./SearchInput";
 
 class App extends React.Component {
 
     //callback method and  <SearchInput onSearchSubmit is props pass from SearchInput
-    onSearchSubmit = (entry) => {
-        console.log(entry);
+    async onSearchSubmit (entry)  {
+        //console.log(entry);
+        const response = await axios.get(`https://pixabay.com/api/?key=29892836-f8039ee2d429fb39538e5e8e3&q=${entry}&image_type=photo&pretty=true`)
+        console.log(response.data.hits);
     }
     render(){
          return (
@@ -15,6 +18,5 @@ class App extends React.Component {
         )
     }
 }
-
 
 export default App;
