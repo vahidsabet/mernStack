47b03b238e6a27840ccf9a9bd8cc29b81a1d06e1
unknown <vahid.sabetumz@gmail.com>
2022-09-13 12:50:23 +0430

    setting The state and this object arrow function with async

--- image-list/src/components/App.js
+++ image-list/src/components/App.js
@@ -4,16 +4,21 @@ import SearchInput from "./SearchInput";
 
 class App extends React.Component {
 
+
+    state = { images: [] }
+
     //callback method and  <SearchInput onSearchSubmit is props pass from SearchInput
-    async onSearchSubmit (entry)  {
+    onSearchSubmit = async (entry) => {
         //console.log(entry);
         const response = await axios.get(`https://pixabay.com/api/?key=29892836-f8039ee2d429fb39538e5e8e3&q=${entry}&image_type=photo&pretty=true`)
-        console.log(response.data.hits);
+        //console.log(response.data.hits);
+        this.setState({images:response.data.hits})
     }
     render(){
          return (
             <div className="ui container" style={{marginTop:'30px'}}>
                 <SearchInput onSearchSubmit={this.onSearchSubmit}/>
+                We have {this.state.images.length} images
             </div>
         )
     }
