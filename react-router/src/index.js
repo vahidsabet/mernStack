4fac759c75b3ab3cca7b4abac4c5151a26cedd2b
unknown <vahid.sabetumz@gmail.com>
2022-09-15 20:50:34 +0430

    Redux state manager new version

--- react-router/src/index.js
+++ react-router/src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {configureStore} from '@reduxjs/toolkit';
+import { Provider } from 'react-redux'; // a 
+import rootReducer from './reducer/rootReducer';
+
+const store = configureStore({reducer:rootReducer});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
