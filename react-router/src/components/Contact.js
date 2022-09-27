f520738de308f140dadb8fcaeb6ef6e5580ff054
unknown <vahid.sabetumz@gmail.com>
2022-09-27 17:53:55 +0330

    Added mapStateToProps func and call cards from state

--- react-router/src/components/Contact.js
+++ react-router/src/components/Contact.js
@@ -1,25 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Modal from "./Modal";
+// import Modal from "./Modal";
 import { connect } from "react-redux";
 
-const Contact = (props)=>{
-
+const Contact = ({cards})=>{
+    //console.log(props.cards);
    /* setTimeout(()=>{
         props.history.push('/about')
     },1000)*/
     return(<div>
-        <Modal />
-            <div className="ui raised very padded text container segment" style={{marginTop:'80px'}}>
-            <Link to="/vahid" className="ui header" >Vahid</Link>
-            <p>Contact vahid</p>
-            </div>
-            <div className="ui raised very padded text container segment" style={{marginTop:'80px'}}>
+        {/* <Modal /> */}
+            {
+                cards.map(card => {
+                    return(
+                        <div className="ui raised very padded text container segment"
+                         style={{marginTop:'80px'}}
+                         key={card.id}
+                         >
+                            <Link to={`/${card.title}`} className="ui header" >{card.title}</Link>
+                            <p>{card.body}</p>
+                        </div>
+                    )
+                })
+            }
+            {/* <div className="ui raised very padded text container segment" style={{marginTop:'80px'}}>
             <Link to="/M" className="ui header" >M</Link>
             <p>Contact M</p>
-            </div>
+            </div> */}
         </div>
     )
 }
+
+//Connect to redux via function 
+const mapStateToProps = (state) => {
+    return {
+        cards: state.cards  //get cards from initialState in reducer
+    }
+};
+
 //connect contact to redux store
-export default connect()(Contact);
+export default connect(mapStateToProps)(Contact);
