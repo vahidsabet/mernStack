cead7cac4afbb739f7598e31318d705537c2ff60
unknown <vahid.sabetumz@gmail.com>
2022-09-28 16:19:01 +0330

    Context Consumer in both function and class component

--- contextapi/src/components/Navbar.js
+++ contextapi/src/components/Navbar.js
@@ -3,23 +3,30 @@ import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends React.Component {
     //access to all data in the context. consume ThemeContext in contextType:a way to use context outside of render method. 
-    static contextType = ThemeContext;
+    // static contextType = ThemeContext;
 
     render(){
-
-        const { isDarkTheme, darkTheme, lightTheme} = this.context; //this.context reach out from contextType object
-        const theme = isDarkTheme ? darkTheme : lightTheme;
+        
         return (
-            <nav style={{ background: theme.background,color:theme.text, height: "120px" }}>
-                <h2 className='ui centered header' style={{ textAlign: 'center'}}>
-                    Ayeezh bot
-                </h2>
-                <div className='ui three buttons'>
-                    <button className='ui button'>Overview</button>
-                    <button className='ui button'>Contact</button>
-                    <button className='ui button'>Support</button>
-                </div>
-            </nav>
+            <ThemeContext.Consumer>
+            {(context)=>{
+                const { isDarkTheme, darkTheme, lightTheme} = context; //this.context reach out from contextType object
+                const theme = isDarkTheme ? darkTheme : lightTheme;
+                return(
+                    <nav style={{ background: theme.background,color:theme.text, height: "120px" }}>
+                        <h2 className='ui centered header' style={{ textAlign: 'center'}}>
+                            Ayeezh bot
+                        </h2>
+                        <div className='ui three buttons'>
+                            <button className='ui button'>Overview</button>
+                            <button className='ui button'>Contact</button>
+                            <button className='ui button'>Support</button>
+                        </div>
+                    </nav>
+                )
+            }}
+            </ThemeContext.Consumer>
+            
         )
     }
 }
