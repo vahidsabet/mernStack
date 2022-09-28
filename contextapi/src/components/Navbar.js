3adde36f8b598c2639575733ae4667609d5874f5
unknown <vahid.sabetumz@gmail.com>
2022-09-28 13:13:37 +0330

    React Context implementation using context

--- contextapi/src/components/Navbar.js
+++ contextapi/src/components/Navbar.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends React.Component {
+    //access to all data in the context
+    static contextType = ThemeContext;
+
     render(){
+
+        const { isDarkTheme, darkTheme, lightTheme} = this.context; //get from context object
+        const theme = isDarkTheme ? darkTheme : lightTheme;
         return (
-            <nav>
-                <h2 className='ui centered header'>
+            <nav style={{ background: theme.background,color:theme.text, height: "120px" }}>
+                <h2 className='ui centered header' style={{ textAlign: 'center'}}>
                     Ayeezh bot
                 </h2>
                 <div className='ui three buttons'>
